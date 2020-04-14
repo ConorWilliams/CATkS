38d26399eaa6bca084c5848a09843536b332d24b
Conor <conorwilliams@outlook.com>
2020-04-14 11:00:22 +0100

    bfgs working direction

--- src/L_BFGS.hpp
+++ src/L_BFGS.hpp
@@ -7,28 +7,85 @@
 
 using Vector = Eigen::Array<double, Eigen::Dynamic, 1>;
 
-template <std::size_t M> class CoreLBFGS {
+template <long M> class CoreLBFGS {
   private:
+    static_assert(M > 0, "Invalid amount of history");
+
     Eigen::Array<double, Eigen::Dynamic, M> m_s;
     Eigen::Array<double, Eigen::Dynamic, M> m_y;
     Eigen::Array<double, Eigen::Dynamic, M> m_rho;
 
+    Vector m_prev_pos;
+    Vector m_prev_grad;
+
     Vector m_a;
 
     std::size_t m_dims;
-    std::size_t k;
+    long m_k;
 
   public:
     CoreLBFGS(std::size_t dims)
-        : m_s{dims, M}, m_y{dims, M}, m_rho{dims, M}, m_a{dims}, m_dims{dims},
-          k{0} {}
+        : m_s{dims, M}, m_y{dims, M}, m_rho{dims, M}, m_prev_pos{dims},
+          m_prev_grad{dims}, m_a{M}, m_dims{dims}, m_k{0} {}
 
-    Vector step(Vector const &pos, Vector const &grad) {
-        Vector q = grad;
+    inline void clear() { m_k = 0; }
 
-        q = pos + grad;
+    /**
+     * Computes the H_k g_k product using the L-BFGS two-loop recursion
+     *
+     * param q      p_k vector to write output to
+     * param pos    x_k current coordinate
+     * param grad   g-k current gradient
+     */
+    inline void operator()(Vector &q, Vector const &pos, Vector const &grad) {
+        q = grad;
 
-        return q;
+        long idx = (m_k - 1) % M;
+
+        // compute k-1 th y,s,rho
+        if (m_k > 0) {
+            m_s.col(idx) = pos - m_prev_pos;
+            m_y.col(idx) = grad - m_prev_grad;
+            m_rho(idx) = 1 / (m_s.col(idx) * m_y.col(idx)).sum();
+        }
+
+        long incr = m_k - M;
+        long bound = M;
+
+        if (m_k <= M) {
+            incr = 0;
+            bound = m_k;
+        }
+
+        // loop 1
+        for (long i = bound - 1; i >= 0; --i) {
+            long j = (i + incr) % M;
+            // std::cout << "one: " << j << std::endl;
+
+            m_a(j) = m_rho(j) * (m_s.col(j) * q).sum();
+            q += m_a(j) * m_y.col(j);
+        }
+
+        // scaling
+        if (m_k > 0) {
+            q *= 1 / (m_rho(idx) * (m_y.col(idx) * m_y.col(idx)).sum());
+        }
+
+        // loop 2
+        for (long i = 0; i <= bound - 1; ++i) {
+            long j = (i + incr) % M;
+            // std::cout << "two: " << j << std::endl;
+
+            double b = m_rho(j) * (m_y.col(j) * q).sum();
+            q += (m_a(j) - b) * m_s.col(j);
+        }
+
+        m_prev_pos = pos;
+        m_prev_grad = grad;
+
+        ++m_k;
+
+        return;
     }
 
     void dump() { std::cout << m_s << std::endl; }
--- src/L_BFGS.test.cpp
+++ src/L_BFGS.test.cpp
@@ -1,23 +1,71 @@
+#pragma once
 
 #include <iostream>
 
 #include "L_BFGS.hpp"
+#include <cmath>
+
+auto grad(Eigen::ArrayXd const &pos) {
+    auto x = pos(0);
+    auto y = pos(1);
+
+    auto x2 = 4 * x * (x * x + y - 11) + 2 * (x + y * y - 7);
+
+    auto y2 = 2 * (x * x + y - 11) + 4 * y * (x + y * y - 7);
+
+    return Eigen::Array<double, 2, 1>{x2, y2};
+}
+
+auto f(Eigen::ArrayXd const &pos) {
+    auto x = pos(0);
+    auto y = pos(1);
+
+    return std::pow(x * x + y - 11, 2) + std::pow(x + y * y - 7, 2);
+}
+
+auto f2(Eigen::ArrayXd const &pos) { return grad(pos).matrix().squaredNorm(); }
 
 int main() {
 
-    constexpr int N = 8;
+    constexpr int dims = 2;
 
-    CoreLBFGS<8> core(N);
+    Eigen::ArrayXd pos(dims);
 
-    Eigen::ArrayXd in(N);
+    CoreLBFGS<10> core(pos.size());
 
-    for (int i = 0; i < N; ++i) {
-        in(i) = i;
+    pos(0) = 3.25;
+    pos(1) = -0.9;
+
+    std::cout << std::endl;
+
+    int count = 0;
+
+    Eigen::ArrayXd step;
+
+    while (f2(pos) > 0.001) {
+        count++;
+
+        core(step, pos, grad(pos));
+
+        // while (f2(pos - step) > f2(pos) + 0.001 * (step * grad(pos)).sum()) {
+        //     std::cout << "yes" << std::endl;
+        //     step /= 1.5;
+        // }
+
+        auto norm = std::sqrt((step * step).sum());
+        double max = 1;
+        if (norm > max) {
+            std::cout << "yes" << std::endl;
+            step /= norm * max;
+        }
+
+        // std::cout << "step: " << step.transpose() << std::endl;
+
+        pos -= step;
+
+        std::cout << count << ": " << pos.transpose() << ' ' << f(pos)
+                  << std::endl;
     }
-
-    std::cout << core.step(in, in) << std::endl;
-
-    core.dump();
 
     std::cout << "working" << std::endl;
 
